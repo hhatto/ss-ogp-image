899c2efedf6683e0c55b93ccff3355ed729ec5b2
Hideo Hattori <hhatto.jp@gmail.com>
2021-05-22 22:45:35 +0900

    image serve

--- mod.ts
+++ mod.ts
@@ -1,19 +1,24 @@
-function handleRequest(request) {
+import { Image } from "https://deno.land/x/imagescript@1.2.6/mod.ts";
+
+async function handleRequest(request: Request) {
   const { pathname } = new URL(request.url);
 
+  const binary = await fetch(new URL("base.png", import.meta.url));
+  const baseImage = await Image.decode(binary.body);
+
+  const binaryFont = await fetch(new URL("Roboto-Regular.ttf", import.meta.url);
+  const font = binary.body;
+
+  const helloText = Image.renderText(font, 120, "hello", 0xffffffff);
+  baseImage.composite(helloText, 30, 100);
+
+  const encoded = await baseImage.encode(1);
+
   return new Response(
-    `<body
-      align="center"
-      style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
-    >
-      <h1>Return JSON and/or HTML Example</h1>
-      <p>
-        ${pathname}
-      </p>
-    </body>`,
+    encoded,
     {
       headers: {
-        "content-type": "text/html; charset=UTF-8",
+        "content-type": "image/png",
       },
     },
   );
