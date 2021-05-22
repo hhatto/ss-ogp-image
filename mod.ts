c996bdfc3ad1de9af2035831b7f12888dc729059
Hideo Hattori <hhatto.jp@gmail.com>
2021-05-22 23:58:23 +0900

    wip

--- mod.ts
+++ mod.ts
@@ -5,35 +5,11 @@ async function handleRequest(request: Request) {
 
   console.log(import.meta.url);
   const binaryURL = new URL("base.png", import.meta.url);
-  const binary = await fetch(binaryURL).then(resp => resp.body)
-    .then(body => {
-      const reader = body?.getReader();
-
-      return new ReadableStream({
-        start(controller) {
-          return pump();
-
-          function pump(): any {
-            return reader?.read().then(({ done, value }) => {
-              // When no more data needs to be consumed, close the stream
-              if (done) {
-                controller.close();
-                return;
-              }
-
-              // Enqueue the next data chunk into our target stream
-              controller.enqueue(value);
-              return pump();
-            });
-          }
-        }
-      })
-    });
+  const binary = await fetch(binaryURL).then(resp => resp.arrayBuffer());
   const baseImage = await Image.decode(binary);
 
   const binaryFontURL = new URL("Roboto-Regular.ttf", import.meta.url);
   const binaryFont = await fetch(binaryFontURL).then(res => res.arrayBuffer());
-  // const font = await binaryFont.getReader().read();
 
   const helloText = Image.renderText(new Uint8Array(binaryFont), 120, "hello", 0xffffffff);
   baseImage.composite(helloText, 30, 100);
